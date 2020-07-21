4b6754e7111c4a3bd2c5df3c266bda3fe6e11891
rudyjm3 <rudyjm3@yahoo.com>
2020-07-21 10:17:40 -0400

    js script for toggle menu button for links to cal. height

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
-navToggle.addEventListener
+navToggle.addEventListener('click', function () {
+//  linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+if(containerHeight === 0){
+  linksContainer.style.height = `${linksHeight}px`
+}
+else{
+  linksContainer.style.height = 0;
+}
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
